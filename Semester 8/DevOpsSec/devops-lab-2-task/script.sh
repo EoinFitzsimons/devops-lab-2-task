da0a2c1a87b41102ff038fad8270d513d743f7f6
Eoin Fitzsimons <eoin00fitzsimons@gmail.com>
2026-01-29 11:25:56 +0000

    Update script.sh to enhance README and .gitignore management

--- Semester 8/DevOpsSec/devops-lab-2-task/script.sh
+++ Semester 8/DevOpsSec/devops-lab-2-task/script.sh
@@ -17,3 +17,15 @@ touch .gitignore
 echo ".gitignore" >> .gitignore
 echo fol_1 >> .gitignore
 echo fol_2 >> .gitignore
+chmod 600 fol_1/1_1.txt
+chmod 600 fol_1/1_3.txt
+chmod 600 fol_2/2_1.txt
+chmod 600 fol_2/2_3.txt
+chmod 777 fol_1/1_2.txt
+chmod 777 fol_1/1_2.txt
+touch README.md
+echo "# Week 2 Lab – Bash & Git" > README.md
+echo Eoin Fitzsimons X23151374 >> README.md
+echo [Eoin's GitHub](https://github.com/EoinFitzsimons) >> README.md
+echo --- >> README.md
+echo "To run the script.sh run: \`bash script.sh\`" >> README.md
